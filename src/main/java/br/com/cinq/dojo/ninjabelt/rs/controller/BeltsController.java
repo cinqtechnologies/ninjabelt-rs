b7e956028bca5878f4278cf2757cabdc72997855
Rômero Ricardo <r.sousa@cinq.com.br>
2018-12-10 01:12:08 -0200

    Adding PUT method for adding belts to team

--- src/main/java/br/com/cinq/dojo/ninjabelt/rs/controller/BeltsController.java
+++ src/main/java/br/com/cinq/dojo/ninjabelt/rs/controller/BeltsController.java
@@ -6,7 +6,9 @@ import br.com.cinq.dojo.ninjabelt.rs.transform.BeltsTransformer;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -37,5 +39,19 @@ public class BeltsController {
       return ResponseEntity.notFound().build();
     }
     return ResponseEntity.ok(presentersBelts);
+  }
+
+  @RequestMapping(
+      value = "/{teamName}/{beltColor}",
+      method = RequestMethod.PUT
+  )
+  public ResponseEntity<Void> addBeltToTeam(
+      final @PathVariable("teamName") String teamName,
+      final @PathVariable("beltColor") String beltColor
+  ) {
+    if (beltsService.addBeltToTeam(teamName, beltColor)) {
+      return ResponseEntity.ok().build();
+    }
+    return ResponseEntity.noContent().build();
   }
 }
--- src/main/java/br/com/cinq/dojo/ninjabelt/rs/service/BeltsService.java
+++ src/main/java/br/com/cinq/dojo/ninjabelt/rs/service/BeltsService.java
@@ -2,9 +2,11 @@ package br.com.cinq.dojo.ninjabelt.rs.service;
 
 import br.com.cinq.dojo.ninjabelt.rs.model.Belt;
 import br.com.cinq.dojo.ninjabelt.rs.model.ListOfBelts;
+import br.com.cinq.dojo.ninjabelt.rs.model.Team;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -15,6 +17,15 @@ import org.springframework.stereotype.Service;
 public class BeltsService {
 
   private final MongoTemplate template;
+
+  private static final List<Belt> AVAILABLE_BELTS =
+      Arrays.asList(
+          buildBelt("1", "white"),
+          buildBelt("2", "yellow"),
+          buildBelt("3", "orange"),
+          buildBelt("4", "brown"),
+          buildBelt("5", "black")
+      );
 
   public BeltsService(final MongoTemplate template) {
     this.template = template;
@@ -31,20 +42,13 @@ public class BeltsService {
     final List<Belt> belts = new ArrayList<>();
 
     list.setTeamName("presenters");
-
-    final Belt white = buildBelt("1", "white");
-    final Belt yellow = buildBelt("2", "yellow");
-    final Belt red = buildBelt("3", "orange");
-    final Belt brown = buildBelt("4", "brown");
-    final Belt black = buildBelt("5", "black");
-
-    belts.addAll(Arrays.asList(white, yellow, red, brown, black));
+    belts.addAll(AVAILABLE_BELTS);
 
     list.setBelts(belts);
     return list;
   }
 
-  private Belt buildBelt(final String id, final String name) {
+  private static Belt buildBelt(final String id, final String name) {
     final Belt belt = new Belt();
     belt.setId(id);
     belt.setName(name);
@@ -53,6 +57,47 @@ public class BeltsService {
 
 
   public ListOfBelts getBeltForTeam(final String teamName) {
-    return template.findOne(Query.query(Criteria.where("teamName").is(teamName)), ListOfBelts.class);
+    return template
+        .findOne(Query.query(Criteria.where("teamName").is(teamName)), ListOfBelts.class);
+  }
+
+  public boolean addBeltToTeam(String teamName, String beltColor) {
+    final ListOfBelts listOfBelts =
+        template.findOne(Query.query(Criteria.where("teamName").is(teamName)), ListOfBelts.class);
+    if (listOfBelts == null) {
+      if (template.exists(Query.query(Criteria.where("name").is(teamName)), Team.class) &&
+          AVAILABLE_BELTS.stream().anyMatch(belt -> belt.getName().equalsIgnoreCase(beltColor))
+      ) {
+        final ListOfBelts persistingListOfBelts = new ListOfBelts();
+        persistingListOfBelts.setTeamName("teamName");
+        persistingListOfBelts.setBelts(
+            AVAILABLE_BELTS.stream()
+                .filter(belt -> belt.getName().equalsIgnoreCase(beltColor)).collect(Collectors.toList())
+        );
+        return template.save(persistingListOfBelts) != null;
+      } else {
+        return false;
+      }
+    } else if (AVAILABLE_BELTS.stream().anyMatch(belt -> belt.getName().equalsIgnoreCase(beltColor))) {
+      if (listOfBelts.getBelts()
+          .stream().map(Belt::getName)
+          .noneMatch(beltColor::equalsIgnoreCase)) {
+        List<Belt> belts = listOfBelts.getBelts();
+        final Belt persistingBelt = AVAILABLE_BELTS.stream()
+            .filter(belt -> belt.getName().equalsIgnoreCase(beltColor))
+            .findAny()
+            .orElse(new Belt("???", beltColor));
+        if (belts == null) {
+          belts = new ArrayList<>();
+          belts.add(persistingBelt);
+          listOfBelts.setBelts(belts);
+        } else {
+          listOfBelts.getBelts().add(persistingBelt);
+        }
+        template.save(listOfBelts);
+      }
+      return true;
+    }
+    return false;
   }
 }
